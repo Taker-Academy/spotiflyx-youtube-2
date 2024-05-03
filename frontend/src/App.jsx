405e53d7dce071224c29c326177b7d82e5e9fb4a
Elias <elias84pujol@gmail.com>
2024-05-03 13:24:49 +0200

    [FIX] home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,44 @@
 import React from 'react';
+import '../css/home.css';
+import '../css/menu_home.css';
 
 function App() {
   return (
-    <nav>
-      <div className="app-container">
-        <div className="container">
-          <a href="#" className="title">Spotiflyx</a>
-          <div className="search-bar">
-            <i className="fas fa-search"></i>
-            <input type="search" placeholder="Rechercher" />
+    <div>
+      <header>
+        <nav>
+          <div className="app-container">
+            <div className="container">
+              <a href="#" className="title">Spotiflyx</a>
+              <div className="search-bar">
+                <i className="fas fa-search"></i>
+                <input type="search" placeholder="Rechercher" />
+              </div>
+              <div className="button">
+                <img src="../images/undraw_pic_profile_re_7g2h.svg" className="avatar" alt="Avatar" />
+              </div>
+            </div>
           </div>
-          <div className="button">
-            <a className="link" href="/auth/login">Connexion</a>
-            <button className="btn" onClick={() => window.location.href='/auth/register'}>
-              Créer un compte
-            </button>
-          </div>
+        </nav>
+      </header>
+      <body>
+        <div className="wrapper">
+          <nav id="sidebar">
+            <div className="title">Side Menu</div>
+            <ul className="list-items">
+                <li><a href="#"><i className="fas fa-home">Home</i></a></li>
+                <li><a href="#"><i className="fas fa-sliders-h">Clients</i></a></li>
+                <li><a href="#"><i className="fas fa-address-book">Services</i></a></li>
+                <li><a href="#"><i className="fas fa-cog">Settings</i></a></li>
+                <li><a href="#"><i className="fas fa-stream">Features</i></a></li>
+                <li><a href="#"><i className="fas fa-user">About us</i></a></li>
+                <li><a href="#"><i className="fas fa-globe-asia">Languages</i></a></li>
+                <li><a href="#"><i className="fas fa-envelope">Contact us</i></a></li>
+            </ul>
+          </nav>
         </div>
-      </div>
-    </nav>
+      </body>
+    </div>
   );
 }
 
