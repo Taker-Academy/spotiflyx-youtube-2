9a1f0469f7456adaba525241c5acb8246c5ab534
Elias <elias84pujol@gmail.com>
2024-05-03 15:42:36 +0200

    [FIX] menu home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,24 +20,20 @@ function App() {
             </div>
           </div>
         </nav>
-      </header>
-      <body>
-        <div className="wrapper">
-          <nav id="sidebar">
-            <div className="title">Side Menu</div>
-            <ul className="list-items">
-                <li><a href="#"><i className="fas fa-home">Home</i></a></li>
-                <li><a href="#"><i className="fas fa-sliders-h">Clients</i></a></li>
-                <li><a href="#"><i className="fas fa-address-book">Services</i></a></li>
-                <li><a href="#"><i className="fas fa-cog">Settings</i></a></li>
-                <li><a href="#"><i className="fas fa-stream">Features</i></a></li>
-                <li><a href="#"><i className="fas fa-user">About us</i></a></li>
-                <li><a href="#"><i className="fas fa-globe-asia">Languages</i></a></li>
-                <li><a href="#"><i className="fas fa-envelope">Contact us</i></a></li>
-            </ul>
-          </nav>
+        <div className="menu">
+          <div className="wrapper">
+            <nav id="sidebar">
+              <div className="title">Side Menu</div>
+              <ul className="list-items">
+                  <li><a href="#"><i className="fas fa-home">Home</i></a></li>
+                  <li><a href="#"><i className="fas fa-cog">Parametres</i></a></li>
+                  <li><a href="#"><i className="fas fa-user">A propos</i></a></li>
+                  <li><a href="#"><i className="fas fa-envelope">Contactez nous</i></a></li>
+              </ul>
+            </nav>
+          </div>
         </div>
-      </body>
+      </header>
     </div>
   );
 }
